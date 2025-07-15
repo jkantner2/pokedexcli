af1421f4226c43c8a3a764b0c6baac10a6d6e060
jkantner2 <kantnerj2@gmail.com>
2025-07-14 23:19:33 -0400

    create startRepl method. initiates REPL. tests cleanInput function with stdio from REPL input.

--- repl.go
+++ repl.go
@@ -2,7 +2,24 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"fmt"
+	"os"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		cleanText := cleanInput(scanner.Text())
+		if len(cleanText) == 0 {
+	
+		}
+		firstWord := cleanText[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+}
 
 func cleanInput(text string)[]string {
 	words := strings.ToLower(text)
